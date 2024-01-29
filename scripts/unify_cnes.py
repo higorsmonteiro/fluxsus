7bc4c5e1dead55da91a226e39fac4b72f10e1b05
higorsmonteiro <higormonteiros@gmail.com>
2024-01-29 16:13:41 -0300

    changes in the processing of cnes files

--- scripts/unify_cnes.py
+++ scripts/unify_cnes.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("..")
 
 import glob
+from tqdm import tqdm
 import pandas as pd
 from fluxsus.DBFIX import DBFIX
 
@@ -37,28 +38,36 @@ def agg_cnes(init_period : str, final_period : str, input_folder : str):
 
     init_index = [idx for idx, s in enumerate(list_of_files) if init_period in s][0]
     final_index = [idx for idx, s in enumerate(list_of_files) if final_period in s][0]
+    if final_index is None:
+        raise Exception(f"no file {final_period} was found.")
     list_of_files = list_of_files[init_index:final_index+1]
-
+    
     cnes_df = []
-    for fname in list_of_files:
+    for fname in tqdm(list_of_files):
         cur_df = DBFIX(fname, codec='latin').to_dataframe()
         cnes_df.append( cur_df )
     cnes_df = pd.concat(cnes_df, axis=0)
 
     # -- keep the most recent record for each CNES
+    min_comp = cnes_df.groupby("CNES")["COMPETEN"].min().reset_index()
+    max_comp = cnes_df.groupby("CNES")["COMPETEN"].max().reset_index()
+    cnes_min = dict(zip(min_comp["CNES"], min_comp["COMPETEN"]))
+    cnes_max = dict(zip(max_comp["CNES"], max_comp["COMPETEN"]))
     cnes_df = cnes_df.sort_values(by=["CNES", "COMPETEN"], ascending=False).drop_duplicates(subset=["CNES"], keep='first')
+    cnes_df["COMPETEN_MIN"] = cnes_df["CNES"].apply(lambda x: cnes_min[x])
+    cnes_df["COMPETEN_MAX"] = cnes_df["CNES"].apply(lambda x: cnes_max[x])
     return cnes_df
 
 if __name__=="__main__":
     # -- change any path according to your own inputs and outputs.
     datapath = os.path.join(os.environ["HOMEPATH"], "Documents", "data")
     input_folder = os.path.join(datapath, "opendatasus", "cnes", "DBF")
-    cnes_df = agg_cnes("STCE1001", "STCE2312", input_folder)
+    cnes_df = agg_cnes("STCE0801", "STCE2312", input_folder)
 
     cnespath = os.path.join(datapath, "opendatasus", "cnes")
     cnesgeo_df = pd.read_csv(os.path.join(cnespath, "cnes_202311091005.csv"), delimiter=";")
     cnesgeo_df['CNES'] = cnesgeo_df['cnes'].apply(lambda x: f"{x:7.0f}".replace(" ", "0"))
 
     cnes_df = cnes_df.merge(cnesgeo_df[["CNES", "latitude", "longitude"]], on="CNES", how="left")
-    cnes_df.to_parquet(os.path.join(os.path.join(datapath, "opendatasus", "cnes", "cnes_st_1001_2312.parquet")))
+    cnes_df.to_parquet(os.path.join(os.path.join(datapath, "opendatasus", "cnes", "cnes_st_0801_2312.parquet")))
     
