ce4eed59354dbafc0f02b4e72ed0536ff5145942
higorsmonteiro <higormonteiros@gmail.com>
2024-02-14 22:40:37 -0300

    updt

--- scripts/create_networks.py
+++ scripts/create_networks.py
@@ -8,27 +8,6 @@ import geopandas as gpd
 import fluxsus.fluxnets.fnets_utils as futils
 from fluxsus.fluxnets.fluxnets import CityFlux, CityHospitalFlux
 
-#def create_citynet(cnes_df, geodata_df, inicial, final, output):
-#    sih_df = []
-#    for year in range(int(inicial), int(final)+1):
-#        cur_df = pd.read_parquet(os.path.join(sihpath, f"RDCE_{year}.parquet"))
-#        sih_df.append(cur_df)
-#    sih_df = pd.concat(sih_df)
-#
-#    # -- generate network
-#    cityflux = CityFlux(cnes_df, geodata_df)
-#    cityflux.define_network().calculate_fluxes(sih_df).to_gml(output)
-
-#def create_cityhospitalnet(cnes_df, geodata_df, inicial, final, output):
-#    sih_df = []
-#    for year in range(int(inicial), int(final)+1):
-#        cur_df = pd.read_parquet(os.path.join(sihpath, f"RDCE_{year}.parquet"))
-#        sih_df.append(cur_df)
-#    sih_df = pd.concat(sih_df)
-#
-#    cityhospitalflux = CityHospitalFlux(cnes_df, geodata_df)
-#    cityhospitalflux.define_network().calculate_fluxes(sih_df).to_gml(output)
-
 # -------------- network creation --------------
 
 # -- obs: need to convert DBFs to parquet before running this script.
@@ -54,7 +33,7 @@ futils.create_citynet(sihpath, cnes_df, geodata_df, f'RDCE2001', f'RDCE2306', ou
 
 # ---- per year
 for yy in [f'{n}' for n in range(10, 23+1)]:
-    futils.create_citynet(sihpath, cnes_df, geodata_df, f'RDCE{yy}01', f'RDCE{yy}12', output=os.path.join(output, f"cityfluxnet_slidingwindow_{yy}01_{yy}12.gml"))
+    futils.create_citynet(sihpath, cnes_df, geodata_df, f'RDCE{yy}01', f'RDCE{yy}12', output=os.path.join(output, f"cityfluxnet_agg_{yy}01_{yy}12.gml"))
 
 # ---- temporal without overlap
 for yy in [f'{n}' for n in range(10, 23+1)]:
@@ -124,24 +103,3 @@ while init_right!='2312':
 
     init_left = f'{years_[yy_left]}{months_[mm_left]}'
     init_right = f'{years_[yy_right]}{months_[mm_right]}'
-
-
-
-#print("Hospitals ... ", end='')
-#create_cityhospitalnet(cnes_df, geodata_df, 2010, 2010, output=os.path.join(output, "CITYHOSPITALFLUX_2010_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2011, 2011, output=os.path.join(output, "CITYHOSPITALFLUX_2011_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2012, 2012, output=os.path.join(output, "CITYHOSPITALFLUX_2012_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2013, 2013, output=os.path.join(output, "CITYHOSPITALFLUX_2013_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2014, 2014, output=os.path.join(output, "CITYHOSPITALFLUX_2014_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2015, 2015, output=os.path.join(output, "CITYHOSPITALFLUX_2015_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2016, 2016, output=os.path.join(output, "CITYHOSPITALFLUX_2016_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2017, 2017, output=os.path.join(output, "CITYHOSPITALFLUX_2017_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2018, 2018, output=os.path.join(output, "CITYHOSPITALFLUX_2018_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2019, 2019, output=os.path.join(output, "CITYHOSPITALFLUX_2019_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2020, 2020, output=os.path.join(output, "CITYHOSPITALFLUX_2020_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2021, 2021, output=os.path.join(output, "CITYHOSPITALFLUX_2021_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2022, 2022, output=os.path.join(output, "CITYHOSPITALFLUX_2022_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2013, 2022, output=os.path.join(output, "CITYHOSPITALFLUX_2013_2022_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2018, 2022, output=os.path.join(output, "CITYHOSPITALFLUX_2018_2022_ALL.gml"))
-#create_cityhospitalnet(cnes_df, geodata_df, 2020, 2022, output=os.path.join(output, "CITYHOSPITALFLUX_2020_2022_ALL.gml"))
-#print("done.")
